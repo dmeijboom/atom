4f747387d012e6ef5697d55eda4ae94a8d398834
Dillen Meijboom <dillen@brainhive.nl>
2024-07-10 23:04:25 +0200

    feat: global error handling

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,28 @@ mod ast;
 mod lexer;
 mod parser;
 
-fn main() {
-    let source = fs::read_to_string("main.atom").expect("failed to open main.atom");
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("I/O error: {0}")]
+    IO(#[from] std::io::Error),
+    #[error("ParseError: {0}")]
+    Parse(#[from] parser::Error),
+    #[error("LexError: {0}")]
+    Lex(#[from] lexer::Error),
+}
+
+fn main() -> Result<(), Error> {
+    let source = fs::read_to_string("main.atom")?;
     let chars = source.chars().collect::<Vec<_>>();
 
     let mut lexer = Lexer::new(&chars);
-    let tokens = lexer.lex().expect("lexer failed");
+    let tokens = lexer.lex()?;
 
     let parser = Parser::new(tokens);
-    let stmts = parser.parse().expect("parser failed");
+    let stmts = parser.parse()?;
 
     let output = format!("{:#?}", stmts).replace("    ", "  ");
     println!("{output}");
+
+    Ok(())
 }
